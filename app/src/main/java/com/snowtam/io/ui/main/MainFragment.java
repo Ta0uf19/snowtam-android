27d8dfb10daa17c7269571871af0ac128749a2ae
Aymane <aymanerizke199@gmail.com>
2020-11-21 18:56:36 +0100

    fix edittext_search annimation

--- app/src/main/java/com/snowtam/io/ui/main/MainFragment.java
+++ app/src/main/java/com/snowtam/io/ui/main/MainFragment.java
@@ -1,5 +1,6 @@
 package com.snowtam.io.ui.main;
 
+import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -9,14 +10,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.snowtam.io.R;
 
 public class MainFragment extends Fragment {
 
     private MainViewModel mViewModel;
+
+    //components
+    private MotionLayout motionLayout;
+    private EditText etSearch;
+
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -26,7 +35,15 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_fragment, container, false);
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        etSearch = view.findViewById(R.id.editText_Search);
+        motionLayout = (MotionLayout) view.findViewById(R.id.main);
+
+        etSearch.setOnTouchListener(getOnTouch(view));
+
+
+        return view;
     }
 
     @Override
@@ -34,6 +51,17 @@ public class MainFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private View.OnTouchListener getOnTouch(View view) {
+        return new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(getContext(),"onTouch",Toast.LENGTH_LONG).show();
+                motionLayout.transitionToEnd();
+                return false;
+            }
+        };
     }
 
 }
